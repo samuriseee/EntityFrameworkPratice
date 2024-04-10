bdd136b777f9450ea8d0329dec26d4ecc0a56a9d
Kiet Nguyen <kiet_nguyen@datahouse.com>
2024-04-10 12:52:29 +0700

    Entity framework pratice

---
+++ ConsoleApp1/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    internal class MyDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost;Database=dbs-commdev;User Id=root;password=1234*;Trusted_Connection=False;MultipleActiveResultSets=true");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                var students = context.Students.Where(s => s.Department == "CNTT" && s.Age >= 18 && s.Age <= 20).ToList();
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Department: {student.Department}");
+                }
+            }
+        }
+    } 
+}
+
+
